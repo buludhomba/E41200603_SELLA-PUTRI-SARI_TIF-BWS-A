ce77e6635f64ddd124ca5f59a0b5c9c88503a55b
buludhomba <sellaputrisari9@gmail.com>
2021-03-15 20:37:06 +0700

    first commit

---
+++ person.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Root
+ */
+public class person {
+    
+    
+    
+     public static void main(String[] args) {
+     
+        String fName, lName, stuId, stuStatus;
+        fName = "Lisa ";
+        lName = "Palombo";
+        stuId = "123456789";
+        stuStatus = "Active";
+        
+        System.out.println("Student Name   : " + fName + lName);
+        System.out.println("Student ID     : " + stuId);
+        System.out.println("Student Status : " + stuStatus);
+       
+     
+     
+     }
+    
+}
